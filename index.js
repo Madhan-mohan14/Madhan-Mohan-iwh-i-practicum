6399da8b510bba2a5941d1573ab1ee62a616f1c6
Madhan-mohan14 <madhanmohan1413@gmail.com>
2026-06-07 23:22:07 +0530

    First commit to my Integrating With HubSpot I: Foundations practicum repository.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -7,65 +8,50 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// * Please DO NOT INCLUDE the private app access token in your repo.
+const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+};
 
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
+// ROUTE 1 - Homepage: fetch all video game character contacts and render table
+app.get('/', async (req, res) => {
+    const url = 'https://api.hubapi.com/crm/v3/objects/contacts';
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        const resp = await axios.get(url, {
+            headers,
+            params: { properties: 'character_name,game_title,special_ability', limit: 100 }
+        });
+        const objects = resp.data.results;
+        res.render('homepage', { title: 'Video Game Characters | HubSpot Practicum', objects });
     } catch (error) {
-        console.error(error);
+        console.error(error.response ? error.response.data : error.message);
+        res.status(500).send('Error fetching data from HubSpot.');
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+// ROUTE 2 - Form page: render the form to add a new character
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', {
+        pageTitle: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+    });
 });
-*/
 
+// ROUTE 3 - Form submit: create new contact record, then redirect home
+app.post('/update-cobj', async (req, res) => {
+    const { character_name, game_title, special_ability } = req.body;
+    const url = 'https://api.hubapi.com/crm/v3/objects/contacts';
+    try {
+        await axios.post(url, {
+            properties: { firstname: character_name, character_name, game_title, special_ability }
+        }, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error.response ? error.response.data : error.message);
+        res.status(500).send('Error creating record in HubSpot.');
+    }
+});
 
-// * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
